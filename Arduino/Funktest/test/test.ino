14dc09ea391621f0e98472c1592064f799adc0ab
Qwip <tobias.eden@freenet.de>
2014-05-22 19:26:56 +0200

    update Funk

--- Arduino/Funktest/test/test.ino
+++ Arduino/Funktest/test/test.ino
@@ -1,22 +1,19 @@
-#include <serial
 #include <SPI.h>
 #include <Mirf.h>
 #include <nRF24L01.h>
 #include <MirfHardwareSpiDriver.h>
 
 // NRF24 settings
-#define RFADDR "rec1"
-#define RFCHANNEL 3
+#define RFADDR "Base"
+#define RFCHANNEL 15
 
-#define TADDR "t1"
+byte Nummer[4];
+byte Rec[4];
+byte check1;
+byte check2;
 
-//besser als digitalWrite
-#define CLR(x,y) (x&=(~(1<<y)))
-#define SET(x,y) (x|=(1<<y))
-
-byte Nummer[4]; 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   //init NRF24
   Mirf.spi = &MirfHardwareSpi;
@@ -24,22 +21,88 @@ void setup() {
   Mirf.csnPin = 10;
   Mirf.init();
   Mirf.setRADDR((byte *)RFADDR);
-  Mirf.setTADDR((byte *)TADDR);
   Mirf.payload = 4;
   Mirf.channel = RFCHANNEL;
   Mirf.config();
   
-  Nummer[1] = 0;
+  Nummer[0] = 0;
+  Nummer[1] = 4;
+  Nummer[2] = 8;
+  Nummer[3] = 12;
+  
+  Rec[0] = 0;
+  Rec[1] = 0;
+  Rec[2] = 0;
+  Rec[3] = 0;
 }
 
 
-void loop(){
+// send and repeat data until ack
+// save ack data in array ack, ack[0] != 2 signals correct exchange
+void consend(byte data[4])
+{
+  byte ack[4] = {0};
+  ack[0] = 2;
   
-  Nummer[1] = serial.read();
+    
+  Mirf.send(data);
+  while(Mirf.isSending()){
+  }
+    
+  Serial.println("Sending...");
   
-  Mirf.send(Nummer);
-  Serial.println(Nummer[1]);
+  if(Mirf.dataReady())
+  {
+    Mirf.getData(ack);
+    Serial.println("Received Ack");
+  }
+}
+
+
+
+void loop()
+{
   
+  // read char from console:
+        if (Serial.available() > 0) 
+        {
+                // read the incoming byte:
+                Nummer[0] = Serial.read();
+
+                // say what you got:
+                Serial.println("I send: ");
+                
+                Serial.print(Nummer[0], DEC);
+                Serial.print(" ");
+                Serial.print(Nummer[1], DEC);
+                Serial.print(" ");
+                Serial.print(Nummer[2], DEC);
+                Serial.print(" ");
+                Serial.println(Nummer[3], DEC);
+        }
+        
+  //send char
+  if(Nummer[0] != check1)
+    consend(Nummer);
+    
+  //receive without ack
+  if(Mirf.dataReady())
+  {
+    Mirf.getData(Rec);
+    if(Rec[0]!=check2)
+    {
+      Serial.println("Received:");
+      for(int i=0; i<4; i++)
+      {
+        Serial.print(Rec[i]);
+        Serial.print(" ");
+      }
+      Serial.println();
+    }
+  }
+  Nummer[0] = check1;
+  Rec[0] = check2;
+  delay(500);
 }
   
   
--- Arduino/Funktest/test2/test2.ino
+++ Arduino/Funktest/test2/test2.ino
@@ -4,45 +4,69 @@
 #include <MirfHardwareSpiDriver.h>
 
 // NRF24 settings
-#define RFADDR "rec1"
-#define RFBASE "rec0"
-#define RFCHANNEL 3
-
-#define TADDR "t1"
-#define TBASE "t0"
+#define RFADDR "Bloon"
+#define RFCHANNEL 15
 
 //besser als digitalWrite
 #define CLR(x,y) (x&=(~(1<<y)))
 #define SET(x,y) (x|=(1<<y))
 
 byte Nummer[4]; 
+byte ack[4] = {1};
+byte check;
 
 void setup() {
-  //Serial.begin(115200);
+  Serial.begin(115200);
   //init NRF24
   Mirf.spi = &MirfHardwareSpi;
   Mirf.cePin = 9;
   Mirf.csnPin = 10;
   Mirf.init();
   Mirf.setRADDR((byte *)RFADDR);
-  Mirf.setTADDR((byte *)TADDR);
   Mirf.payload = 4;
   Mirf.channel = RFCHANNEL;
   Mirf.config();
   
+  Nummer[0] = 0;
   Nummer[1] = 0;
+  Nummer[2] = 0;
+  Nummer[3] = 0;
 }
 
 
 void loop(){
-  Mirf.getData(Nummer);
-  if(Nummer[1] > 6)
-    Nummer[1] = 0;
-  Nummer[1] = Nummer[1] + 1;
-  Mirf.send(Nummer);
   
-  Serial.println(Nummer[1]);
+  if(Mirf.dataReady())
+  {
+    Mirf.getData(Nummer);
+     
+    Mirf.send(ack);
+    while(Mirf.isSending()){
+    }
+    Serial.println("Ack sent");
+  }
   
+  delay(500);
+  
+  if(check != Nummer[0])
+  {
+    Serial.print(Nummer[0], DEC);
+    Serial.print(" ");
+    Serial.print(Nummer[1], DEC);
+    Serial.print(" ");
+    Serial.print(Nummer[2], DEC);
+    Serial.print(" ");
+    Serial.println(Nummer[3], DEC);
+          
+    Mirf.send(Nummer);
+    while(Mirf.isSending()){
+    }
+    
+    Serial.println("Data sent");
+    Serial.println();
+  }
+  check = Nummer[0];
+  delay(100);
 }
   
   
